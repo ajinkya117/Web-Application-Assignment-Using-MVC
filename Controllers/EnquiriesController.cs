4fb058543bec41f525c4f20eb0d6441c9a532bc5
ajinkya117 <57980653+ajinkya117@users.noreply.github.com>
2020-10-08 14:47:22 +0530

    Add files via upload

---
+++ Controllers/EnquiriesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GoAdsIndiaTest;
+
+namespace GoAdsIndiaTest.Controllers
+{
+    public class EnquiriesController : Controller
+    {
+        private GoAdsIndiaEntities db = new GoAdsIndiaEntities();
+
+        // GET: Enquiries
+        public ActionResult Index()
+        {
+            return View(db.Enquiries.ToList());
+        }
+
+        // GET: Enquiries/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enquiry enquiry = db.Enquiries.Find(id);
+            if (enquiry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enquiry);
+        }
+
+        // GET: Enquiries/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Enquiries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name,Email,Mobile,Message")] Enquiry enquiry)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (enquiry.Email == null && enquiry.Name == null && enquiry.Mobile == null && enquiry.Message == null)
+                {
+                     
+                    ModelState.AddModelError("", "Value Can't be empty");
+                }
+                else
+                {
+                    db.Enquiries.Add(enquiry);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                
+            }
+
+            return View(enquiry);
+        }
+
+        // GET: Enquiries/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enquiry enquiry = db.Enquiries.Find(id);
+            if (enquiry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enquiry);
+        }
+
+        // POST: Enquiries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Name,Email,Mobile,Message")] Enquiry enquiry)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enquiry).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(enquiry);
+        }
+
+        // GET: Enquiries/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enquiry enquiry = db.Enquiries.Find(id);
+            if (enquiry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enquiry);
+        }
+
+        // POST: Enquiries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Enquiry enquiry = db.Enquiries.Find(id);
+            db.Enquiries.Remove(enquiry);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
